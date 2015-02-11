037047b507968602fee41e678be6f6a655afb27d
eorodrig <eorodrig@ualberta.ca>
2015-02-11 15:54:11 -0700

    added test for adding travel destination information

--- TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
+++ TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
@@ -34,6 +34,8 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 		
 		String name = "Bill Smith";
 		Date startDate = new Date(100), endDate = new Date(120);
+		
+		//Claim constructor with string name, Date startDate, Date endDate
 		Claim claim = new Claim(name, startDate, endDate);
 		
 		assertTrue("Claim is null", claim != null);
@@ -47,14 +49,34 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 	 * Test that you can't add an end date that occurs after the start date. 
 	 * US01.01.02
 	 */
-	public void testAddInvalidDateClaim(){
+	public void testAddInvalidDate(){
 		
 		String name = "Bill Smith";
 		Date startDate = new Date(120), endDate = new Date(100);
 		Claim claim = new Claim(name, startDate, endDate);
 		
-		assertTrue("Start date occurs after end date", claim.getEndDate.before(claim.getEndDate()));
+		assertTrue("Start date occurs after end date", claim.getStartDate.before(claim.getEndDate()));
 	}
 	
-	
+	/**
+	 * Tests that the claim can add travelDestination 
+	 * US01.02.01
+	 */
+	public void testTravelDestination(){
+		
+		String name = "Bill Smith";
+		Date startDate = new Date(120), endDate = new Date(100);
+		Claim claim = new Claim(name, startDate, endDate);
+		
+		//this method adds the travel destination to a collection (possibly hash table/map)
+		claim.addTravelDestination("Russia","Bear wrestling");
+		
+		//look to see if destination collection is larger than 0
+		assertTrue("Claim has has no Travel Destinations", claim.numberOfDestinations() > 0);
+		
+		//test to see if the contents of the destination collection are correct
+		assertTrue("Claim has invalid Travel Destination", claim.getTravelDestination(0).getDestinationName().equals("Russia"));
+		assertTrue("Claim has invalid description", claim.getTravelDestination(0).getDestinationDescription().equals("Bear wrestling"));
+
+	}
 }
