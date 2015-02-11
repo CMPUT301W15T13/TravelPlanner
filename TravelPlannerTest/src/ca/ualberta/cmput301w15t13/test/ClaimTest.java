f2285fd15d6019be8bd0783207eea6b4e1c27de7
eorodrig <eorodrig@ualberta.ca>
2015-02-11 15:26:16 -0700

    added JUnit file for claim

---
+++ TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
@@ -0,0 +1,29 @@
+package ca.ualberta.cmput301w15t13.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+
+/**
+ * These are the JUnit tests for the claim class
+ * @author eorodrig
+ *
+ */
+public class ClaimTest extends ActivityInstrumentationTestCase2<ClaimActivity> {
+
+	public ClaimTest() {
+		super(ClaimActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	
+	
+	public testAddClaim(){
+		
+	}
+	
+	
+	
+}
