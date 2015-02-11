c2cb5c71359e517f07cfe71e10ced6b4fa3d8982
eorodrig <eorodrig@ualberta.ca>
2015-02-11 16:23:14 -0700

    finished adding claim cases for us01.01.01, us01.01.02, us01.02.01, us01.02.02, us01.06.01

--- TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
+++ TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
@@ -30,32 +30,39 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 	 * Test a regular name and a starting date preceding an end date is accepted 
 	 * US01.01.01
 	 */
-	public void testAddRegularClaim(){
+	public void testMakeRegularClaim(){
 		
 		String name = "Bill Smith";
 		Date startDate = new Date(100), endDate = new Date(120);
 		
 		//Claim constructor with string name, Date startDate, Date endDate
 		Claim claim = new Claim(name, startDate, endDate);
+	
+		
+		claim.addDescription("Trip to Rome");
+
 		
 		assertTrue("Claim is null", claim != null);
 		assertTrue("Claim name is wrong", claim.getName().equals(name));
 		assertTrue("Claim startDate is wrong", claim.getStartDate().equals(startDate));
 		assertTrue("Claim name is wrong", claim.getEndDate().equals(endDate));
+		assertTrue("Claim description is wrong", claim.getDescription().equals("Trip to Rome"));
+		
 	}
 	
 	
+
 	/**
 	 * Test that you can't add an end date that occurs after the start date. 
 	 * US01.01.02
 	 */
-	public void testAddInvalidDate(){
+	public void testUseInvalidDate(){
 		
 		String name = "Bill Smith";
 		Date startDate = new Date(120), endDate = new Date(100);
 		Claim claim = new Claim(name, startDate, endDate);
 		
-		assertTrue("Start date occurs after end date", claim.getStartDate.before(claim.getEndDate()));
+		assertTrue("Invalid Claim: Start date occurs after end date", claim.getEndDate.before(claim.getStartDate()));
 	}
 	
 	/**
@@ -79,4 +86,57 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 		assertTrue("Claim has invalid description", claim.getTravelDestination(0).getDestinationDescription().equals("Bear wrestling"));
 
 	}
+	
+	
+	/**
+	 * Tests that the claim does not add duplicate entries
+	 * US01.02.02
+	 */
+	public void testInvalidTravelDestination(){
+		
+		String name = "Bill Smith";
+		Date startDate = new Date(120), endDate = new Date(100);
+		Claim claim = new Claim(name, startDate, endDate);
+		
+		//this method adds the travel destination to a collection (possibly hash table/map)
+		claim.addTravelDestination("Russia","Bear wrestling");
+	
+		//Uses an exception to catch duplicate entries
+		try {
+			claim.addTravelDestination("Russia","Bear wrestling");
+			fail("Duplicate Travel Destination Entered");
+		}
+		catch (DuplicateException e){
+		
+		}
+	}
+	
+	
+	/**
+	 *Tests that we can edit a claim
+	 * US01.06.01
+	 */
+	public void testEditClaim(){
+		
+		String name = "Bill Smith", name2 = "Joe Brown";
+		Date startDate = new Date(100), startDate2 = new Date(200);
+		Date endDate = new Date(120) , endDate2 = new Date(220);
+		String description = "Claim for trip to Rome", description2 = "trip to Italy";
+		
+		Claim claim = new Claim(name, startDate, endDate);
+		claim.addDescription(description);
+		
+		claim.editName(name2);
+		claim.editStartDate(startDate2);
+		claim.editEndDate(endDate2);
+		claim.editDescription("Trip to Italy");
+		
+		assertTrue("Edited claim is null", claim != null);
+		assertTrue("Edited claim name is wrong", claim.getName().equals(name2));
+		assertTrue("Edited claim startDate is wrong", claim.getStartDate().equals(startDate2));
+		assertTrue("Edited claim name is wrong", claim.getEndDate().equals(endDate2));
+		assertTrue("Edited claim description is wrong", claim.getDescription().equals(description2));
+
+	}
+	
 }
