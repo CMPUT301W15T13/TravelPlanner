41a7b3215006d07dfcb4b75ad6f3835e82cad218
eorodrig <eorodrig@ualberta.ca>
2015-02-11 15:42:09 -0700

    added some test cases for claims

--- TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
+++ TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ClaimTest.java
@@ -1,5 +1,8 @@
 package ca.ualberta.cmput301w15t13.test;
 
+import java.util.Date;
+
+import ca.ualberta.cmput301w15t13.LoginActivity;
 import android.test.ActivityInstrumentationTestCase2;
 
 
@@ -8,10 +11,13 @@ import android.test.ActivityInstrumentationTestCase2;
  * @author eorodrig
  *
  */
-public class ClaimTest extends ActivityInstrumentationTestCase2<ClaimActivity> {
+
+//This is the test for claims
+//the LoginActivity value used will have to change once the classes are finalized
+public class ClaimTest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
 	public ClaimTest() {
-		super(ClaimActivity.class);
+		super(LoginActivity.class);
 	}
 
 	protected void setUp() throws Exception {
@@ -20,10 +26,35 @@ public class ClaimTest extends ActivityInstrumentationTestCase2<ClaimActivity> {
 
 	
 	
-	public testAddClaim(){
+	/**
+	 * Test a regular name and a starting date preceding an end date is accepted 
+	 * US01.01.01
+	 */
+	public void testAddRegularClaim(){
 		
+		String name = "Bill Smith";
+		Date startDate = new Date(100), endDate = new Date(120);
+		Claim claim = new Claim(name, startDate, endDate);
+		
+		assertTrue("Claim is null", claim != null);
+		assertTrue("Claim name is wrong", claim.getName().equals(name));
+		assertTrue("Claim startDate is wrong", claim.getStartDate().equals(startDate));
+		assertTrue("Claim name is wrong", claim.getEndDate().equals(endDate));
 	}
 	
+	
+	/**
+	 * Test that you can't add an end date that occurs after the start date. 
+	 * US01.01.02
+	 */
+	public void testAddInvalidDateClaim(){
+		
+		String name = "Bill Smith";
+		Date startDate = new Date(120), endDate = new Date(100);
+		Claim claim = new Claim(name, startDate, endDate);
+		
+		assertTrue("Start date occurs after end date", claim.getEndDate.before(claim.getEndDate()));
+	}
 	
 	
 }
