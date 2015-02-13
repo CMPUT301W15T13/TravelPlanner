0da6c6d70cd9af34791eada6e31eb306150955f2
lolicatgirls <devito@ualberta.ca>
2015-02-13 00:10:12 -0700

    07.01.01 test case

---
+++ TravelPlannerTest/src/ca/ualberta/cmput301w15t13/test/ExpenseClaimsStatusesTests.java
@@ -0,0 +1,26 @@
+package ca.ualberta.cmput301w15t13.test;
+
+import ca.ualberta.cmput301w15t13.ClaimModel;
+import ca.ualberta.cmput301w15t13.LoginActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ExpenseClaimsStatusesTests extends ActivityInstrumentationTestCase2<ClaimModel>{
+	public ExpenseClaimsStatusesTests() {
+		super(ClaimModel.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	//07.01.01
+	//Claimant submits a claim can no longer edit
+	public void testClaimantSubmit(){
+		LoginActivity.setUserType("claimant"); 
+		Claim claim = new Claim;
+		LoginActivity.submit(claim);
+		assertEquals("Claim is submitted", claim.getStatus(), "Submitted");
+		assertEquals("Claim cannot be editted by claimant", claim.getPermission(), 1);
+	}
+
+}
