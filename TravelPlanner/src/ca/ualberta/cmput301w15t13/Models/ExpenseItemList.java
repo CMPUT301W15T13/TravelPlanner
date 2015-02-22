e1426237dd1e4f5e850d5d5cc31d719e868f8e3a
jihwan1ua <jihwan@ualberta.ca>
2015-02-22 16:27:57 -0700

    Wrote some basic codes for the ExpenseItemList class

---
+++ TravelPlanner/src/ca/ualberta/cmput301w15t13/Models/ExpenseItemList.java
@@ -0,0 +1,38 @@
+// Ji Hwan Kim - 2015/02/22
+
+package ca.ualberta.cmput301w15t13.Models;
+
+import java.util.ArrayList;
+
+public class ExpenseItemList {
+	// First assign the array, ExpenseItemList, to be null
+	ArrayList<ExpenseItem> ExpenseItemList = null;
+	
+	// Initializing the ExpenseItemList
+	public ExpenseItemList()
+	{
+		ExpenseItemList = new ArrayList<ExpenseItem>();
+	}
+	
+	// some methods used to control ExpenseItemList. i.e. add, delete, update, etc
+	public void add(ExpenseItem Expense)
+	{
+		// No need to check for duplicate as there could be multiple identical expenses in a claim
+		ExpenseItemList.add(Expense);
+	}
+	
+	public void delete(ExpenseItem Expense)
+	{
+		if (ExpenseItemList.contains(Expense)==true){
+			// there is an Expense in a list we can delete, then delete
+			ExpenseItemList.remove(Expense);
+		} else if (ExpenseItemList.contains(Expense)==false){
+			// there is no item Expense in the list we can delete
+			return;
+		} else if (ExpenseItemList.isEmpty()==true){
+			// if the list is empty, then return
+			return;
+		}
+	}
+
+}
