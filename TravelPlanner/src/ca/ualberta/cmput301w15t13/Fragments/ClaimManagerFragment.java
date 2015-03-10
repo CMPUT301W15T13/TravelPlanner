26d164cadae92820aebb07b2cf347d281bb5cba3
mfritze <mfritze@ualberta.ca>
2015-03-10 15:13:10 -0600

    Now gets date values from the dialog and sets them to local date vars

--- TravelPlanner/src/ca/ualberta/cmput301w15t13/Fragments/ClaimManagerFragment.java
+++ TravelPlanner/src/ca/ualberta/cmput301w15t13/Fragments/ClaimManagerFragment.java
@@ -21,19 +21,26 @@
 package ca.ualberta.cmput301w15t13.Fragments;
 
 import java.util.Calendar;
+import java.util.Date;
 
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.app.Fragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import ca.ualberta.cmput301w15t13.R;
+import ca.ualberta.cmput301w15t13.Activities.ClaimActivity;
+import ca.ualberta.cmput301w15t13.Controllers.ClaimListSingleton;
+import ca.ualberta.cmput301w15t13.Models.Claim;
+import ca.ualberta.cmput301w15t13.Models.TravelItineraryList;
+import exceptions.InvalidDateException;
+import exceptions.InvalidNameException;
+import exceptions.InvalidUserPermissionException;
 
 /**
  * This fragment is used for creating and editing claims.
@@ -45,11 +52,26 @@ import ca.ualberta.cmput301w15t13.R;
  */
 
 public class ClaimManagerFragment extends Fragment{
-
+	EditText claimName, description;
+	TextView startDateView, endDateView;
+	String nameText, descriptionText;
+	TravelItineraryList itineraryList;
+	Date startDate, endDate;
+	boolean completeFields;
+	
+	//final Calendar startDateCal = Calendar.getInstance(), endDateCal = Calendar.getInstance();
+	
+	
+	//TODO add a back button, in the case they don't want to submit a claim? And maybe
+	//force change what the back button does from this screen, in that it moves to the old fragment
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		itineraryList = new TravelItineraryList();
+		completeFields = false;
+		startDate = new Date();
+		endDate = new Date();
 	}
 
 	@Override
@@ -61,8 +83,11 @@ public class ClaimManagerFragment extends Fragment{
 
 	@Override
 	public void onStart() {
-		// TODO Auto-generated method stub
 		super.onStart();
+		claimName = (EditText) getView().findViewById(R.id.editTextClaimName);
+		description = (EditText) getView().findViewById(R.id.editTextClaimDescription);
+		startDateView = (TextView) getView().findViewById(R.id.textViewStartDate);
+		endDateView = (TextView) getView().findViewById(R.id.textViewEndDate);
 	}
 
 	
@@ -75,20 +100,23 @@ public class ClaimManagerFragment extends Fragment{
 	 */
 	public void openDateDialog(TextView textId) {
 		// Based on http://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext March 06 2015
-		final Calendar myCalendar = Calendar.getInstance();
 		final TextView dateTextView = textId;
+		final Calendar myCalendar = Calendar.getInstance();
 		
 		DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
 		    @Override
 		    public void onDateSet(DatePicker view, int year, int monthOfYear,
 		            int dayOfMonth) {
+		    	// When the positive button is clicked collect the date data
 		        myCalendar.set(Calendar.YEAR, year);
 		        myCalendar.set(Calendar.MONTH, monthOfYear);
 		        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-		        String dateText = Integer.toString(dayOfMonth) + "/" + Integer.toString(monthOfYear)
+		        String dateText = Integer.toString(dayOfMonth) + "/" + Integer.toString(monthOfYear + 1)
 		        		+ "/" + Integer.toString(year);
 		        dateTextView.setText(dateText);
+		        
+		        addDateData(year, monthOfYear, dayOfMonth, dateTextView);
 		    }
 
 		};
@@ -98,11 +126,64 @@ public class ClaimManagerFragment extends Fragment{
                 myCalendar.get(Calendar.DAY_OF_MONTH)).show();
 		        
 	}
+	
+	/**
+	 * Method to escape the scope issue inside the 
+	 * onDateSet method. Simply sets the local value of 
+	 * the start or end date to the local date var.
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param text
+	 */
+	
+	@SuppressWarnings("deprecation")
+	public void addDateData(int year, int month, int day, TextView text){
+		Date date;
+		if(text == startDateView){
+			date = startDate;
+		}else{
+			date = endDate;
+		}
+		date.setDate(day);
+		date.setMonth(month);
+		date.setYear(year);
+		Toast.makeText(getActivity(), Integer.toString(day) + "/" + Integer.toString(month) + "/" + Integer.toString(year), Toast.LENGTH_SHORT).show();
+	}
 
 	public void openDestinationDialog() {
 		DestinationDialogFragment dialog = new DestinationDialogFragment();
 		dialog.show(getFragmentManager(), "TEST TAG");
 	}
 	
+
+	public void createClaim() throws InvalidDateException, InvalidNameException, InvalidUserPermissionException{
+		if(true){ // TODO change to check that all fields are filled, maybe in the update Ref function
+			Claim newClaim = new Claim(this.nameText, startDate, endDate, 
+				this.descriptionText, itineraryList);
+			ClaimListSingleton.getClaimList().add(newClaim);
+			((ClaimActivity) getActivity()).setFragmentToClaimViewer();
+		}else{
+			Toast.makeText(getActivity(), "Fill in all fields before submitting", Toast.LENGTH_SHORT).show();
+		}
+	}
 	
+	/**
+	 * Updates the class level variables to reflect what's in the
+	 * fields in the layout.
+	 */
+	public void updateReferences(){
+		// TODO check fields are not empty
+		nameText = claimName.getText().toString().trim() + ""; //add the "" to check for an empty field
+		descriptionText = description.getText().toString().trim() + ""; 
+		
+		
+		// for loop to parse the destinations and their reasons
+		
+		//TODO should we assert they fill in all fields?
+		if(!nameText.equals("") && descriptionText.equals("") &&
+				startDate != null && endDate != null && itineraryList.size() == 0){
+			this.completeFields = true;
+		}
+	}
 }
