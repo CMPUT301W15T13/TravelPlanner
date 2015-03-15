888c2bfb96bbc12c497a5fb068477b3da1848eac
Ben Hunter <bkhunter@ualberta.ca>
2015-03-15 14:00:57 -0600

    ExpenseItemList updated im UML

--- TravelPlanner/src/ca/ualberta/cmput301w15t13/Models/ExpenseItemList.java
+++ TravelPlanner/src/ca/ualberta/cmput301w15t13/Models/ExpenseItemList.java
@@ -22,72 +22,68 @@ package ca.ualberta.cmput301w15t13.Models;
 
 import java.util.ArrayList;
 
+/*
+ * This class is a container for individual expense
+ * items. It is used by a claim to manage it's
+ * constituent expense items.
+ */
 public class ExpenseItemList {
-	// First assign the array, ExpenseItemList, to be null
+	// First assign the arrayList expenseItemList to be null
 	ArrayList<ExpenseItem> expenseItemList = null;
-	
 	String claimID = null;
 	
 	// Initializing the ExpenseItemList
-	public ExpenseItemList()
-	{
+	public ExpenseItemList() {
 		expenseItemList = new ArrayList<ExpenseItem>();
 	}
 	
 	// Initializing the ExpenseItemList
-	public ExpenseItemList(String ClaimID)
-	{
+	public ExpenseItemList(String ClaimID) {
 		claimID =ClaimID; 
 		expenseItemList = new ArrayList<ExpenseItem>();
 	}
 	
 	// some methods used to control ExpenseItemList. i.e. add, delete, update, etc
-	public void add(ExpenseItem Expense)
-	{
+	public void add(ExpenseItem Expense) {
 		// No need to check for duplicate as there could be multiple identical expenses in a claim
 		expenseItemList.add(Expense);
 	}
 	
-	public void delete(ExpenseItem Expense)
-	{
-		if (expenseItemList.contains(Expense)==true){
+	public void delete(ExpenseItem Expense) {
+		if (expenseItemList.contains(Expense)==true) {
 			// there is an Expense in a list we can delete, then delete
 			expenseItemList.remove(Expense);
-		}
-		
+		}	
 	}
 	
-	public int size(){
+	public int size() {
 		return this.expenseItemList.size();
 	}
 	
-	public boolean isempty(){
+	public boolean isempty() {
 		// this will check if the list is empty or not, return true iff empty else false
-		if (this.expenseItemList.size() == 0){
+		if (this.expenseItemList.size() == 0) {
 			return true;
-		} else {
-			return false;
 		}
+		return false;
 	}
 	
-	public ArrayList<ExpenseItem> getExpenseList(){
+	public ArrayList<ExpenseItem> getExpenseList() {
 		return this.expenseItemList;
 	}
 	
-	public ExpenseItem findExpenseItem(int index){
+	public ExpenseItem findExpenseItem(int index) {
 		// this method might be trimmed down a bit, little help will be nice if want to modify this
 		// check index is valid, this automatically check if list is empty too
-		if (this.expenseItemList.size() > index && index >= 0){
+		if (this.expenseItemList.size() > index && index >= 0) {
 			return(this.expenseItemList.get(index));
-		} else {
-			// input index is wrong or the list is empty
-			return null;
-		}
+		} 
+		// input index is wrong or the list is empty
+		return null;
 	}
 
 	public void notifyListeners() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 	
 
