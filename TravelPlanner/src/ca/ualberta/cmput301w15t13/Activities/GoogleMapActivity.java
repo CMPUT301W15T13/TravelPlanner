a9a0b2d1e1414545f316b60860fe886029ddeb71
mfritze <mfritze@ualberta.ca>
2015-04-06 13:22:22 -0600

    Maps can now be opened

--- TravelPlanner/src/ca/ualberta/cmput301w15t13/Activities/GoogleMapActivity.java
+++ TravelPlanner/src/ca/ualberta/cmput301w15t13/Activities/GoogleMapActivity.java
@@ -1,10 +1,14 @@
 package ca.ualberta.cmput301w15t13.Activities;
 
-import ca.ualberta.cmput301w15t13.R;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import ca.ualberta.cmput301w15t13.R;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public class GoogleMapActivity extends FragmentActivity {
+	private GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
